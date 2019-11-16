9bac4aee7dfd42d7219c6772b35c14b8d0937088
Victoria Chu <victoria.p.chu@berkeley.edu>
2019-11-16 11:16:18 -0800

    markers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from "react-google-maps";
+import * as parksData from "./data/skateboard-parks.json";
 
 function Map() {
     return ( 
         <GoogleMap 
             defaultZoom={10} 
             defaultCenter={{ lat:45, lng:-75}}
-        />
+        >
+		console.log(locationsData)
+	{parksData.features.map(park => (
+		<Marker
+			key={park.properties.PARK_ID}
+			position={{
+				lat:park.geometry.coordinates[1],
+				lng:park.geometry.coordinates[0]
+			}}
+		/>
+	))}
+	
+	</GoogleMap>
     );
 }
 
