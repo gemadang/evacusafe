4b59586d3937f08f98630411cfc346fed9e1358c
Geri Madanguit <geri@rhombuspower.com>
2019-11-16 08:56:55 -0800

    read and write example

--- node/db.js
+++ node/db.js
@@ -1,5 +1,42 @@
 var provider = new firebase.auth.GoogleAuthProvider();
+var fb_db = firebase.database();
 
+//
+// READ
+//
+
+var test = firebase.database().ref('users/');
+test.on('value', function(snapshot) {
+    console.log(test);
+});
+
+//
+// WRITE
+//
+function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('users/' + userId).set({
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+  }
+
+
+firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+}, function(error) {
+    if (error) {
+      // The write failed...
+    } else {
+      // Data saved successfully!
+    }
+});
+
+//
+// GOOGLE AUTH
+//
 function sign_in_with_popup(){
     firebase.auth().signInWithPopup(provider).then(function(result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
